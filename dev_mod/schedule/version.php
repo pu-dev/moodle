72eb0b3f47906e73e98dfb8c7c38febcdfccae60
Przemek Urbanski <purbanski@interia.pl>
2020-04-30 20:45:55 +0200

    Update plugin version

--- dev_mod/schedule/version.php
+++ dev_mod/schedule/version.php
@@ -1,7 +1,7 @@
 <?php
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2019111802;       // The current module version (Date: YYYYMMDDXX)
+$plugin->version   = 2020042901;       // The current module version (Date: YYYYMMDDXX)
 $plugin->requires  = 2019111200;    // Requires this Moodle version
 $plugin->component = 'mod_schedule';     // Full name of the plugin (used for diagnostics)
 $plugin->cron      = 0;
